694b5bc4ba481084645d60f123ce950a7380e4d3
Biprotosh <biprotosh123@gmail.com>
2026-05-20 21:23:39 +0530

    Learnt about setter injection (not a preferred way)

--- src/main/java/com/springboottutorial/store/OrderService.java
+++ src/main/java/com/springboottutorial/store/OrderService.java
@@ -1,13 +1,20 @@
 package com.springboottutorial.store;
 
 public class OrderService {
-    private final PaymentService paymentService;
+    private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService){
+    // constructor injections is the preferred way to inject a dependency in a class
+//    public OrderService(PaymentService paymentService){
+//        this.paymentService = paymentService;
+//    }
+
+    // setter injection to inject a dependency
+    public void setPaymentService(PaymentService paymentService){
         this.paymentService = paymentService;
     }
 
     public void placeOrder(){
+//        System.out.println("problem");
         paymentService.processPayment(10);
     }
 }
--- src/main/java/com/springboottutorial/store/StoreApplication.java
+++ src/main/java/com/springboottutorial/store/StoreApplication.java
@@ -9,7 +9,12 @@ public class StoreApplication {
     public static void main(String[] args) {
 //        SpringApplication.run(StoreApplication.class, args);
 
-        OrderService orderService = new OrderService(new PaytmPaymentService());
+//        OrderService orderService = new OrderService(new PaytmPaymentService());
+        OrderService orderService = new OrderService();
+
+        // not recommended, because what if we forget to set it. It will give NullPointer exception
+        orderService.setPaymentService(new PaytmPaymentService());
+
         orderService.placeOrder();
     }
 }
